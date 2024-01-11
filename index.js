3293ab71fdcbb98cbd5ac89148d689d913f42e3b
sanghvimoksha <111114487+sanghvimoksha@users.noreply.github.com>
2024-01-11 14:29:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,84 @@
+// Variables for buttons
+
+const startStopBtn = document.querySelector("#startStopBtn");
+const restart = document.querySelector("#restart");
+
+// variables for time Value
+
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
+// Variables for leading Zero
+
+let leadingSeconds = 0;
+let leadingMinutes = 0;
+let leadingHours = 0;
+
+
+//Variables for se intervals and times
+
+let timeInterval = "null";
+let timerStatus = "stopped";
+
+// Stop Watch funtion
+function stopWatch(){
+
+seconds++
+
+if(seconds / 60 === 1){
+    seconds = 0;
+    minutes ++;
+
+    if(minutes /60 === 1){
+        minutes = 0;
+        hours ++;
+    }
+}
+
+  if(seconds < 10){
+    leadingSeconds = "0" + seconds.toString();
+  } else {
+    leadingSeconds = seconds;
+  }
+
+  if(minutes < 10){
+    leadingMinutes = "0" + minutes.toString();
+  } else {
+    leadingMinutes = minutes;
+  }
+
+  if(hours < 10){
+    leadingHours = "0" + hours.toString();
+  } else {
+    leadingHours = hours;
+  }
+
+
+  let displayTimer = document.getElementById("timer").innerText = leadingHours + ":" + leadingMinutes + ":" + leadingSeconds;
+}
+
+// 
+
+startStopBtn.addEventListener("click", function(){
+    if (timerStatus === "stopped"){
+        timerInterval = window.setInterval(stopWatch, 1000);
+        document.getElementById("startStopBtn").innerHTML = '<i class="fa-solid fa-pause" id="pause"></i>';
+        timerStatus = "started";
+    }  else{
+        window.clearInterval(timerInterval);
+        document.getElementById("startStopBtn").innerHTML = '<i class="fa-solid fa-play" id="play"></i>';
+        timerStatus = "stopped";
+    }
+});
+
+
+reset.addEventListener("click", function(){
+    window.clearInterval(timerInterval);
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+
+    document.getElementById("timer").innerHTML = "00:00:00";
+
+});
